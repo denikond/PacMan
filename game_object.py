115f6de127d8870ad9b3889b383590092a65d06b
fox <fox@kond.me>
2023-11-26 21:42:38 +0300

    Try animate player

--- game_object.py
+++ game_object.py
@@ -12,18 +12,26 @@ class GameObject(Sprite):
     width: int = 40
     height: int = 40
     color_key: tuple[int, int, int] = (245, 245, 245)
+    current_image: str | None = None
 
     def __init__(self, topleft_x: int, topleft_y: int):
         super().__init__()
+        print(self.sprite_filenames)
+        self.image = {}
         for sprite_filename in self.sprite_filenames:
-            sprite_image_full_path = os.path.join("resources", f"{self.sprite_filename}.{self.sprite_extension}")
-        self.image = scale(load(sprite_image_full_path), (self.width, self.height))
-        self.image.set_colorkey(self.color_key)
-        self.rect = self.image.get_rect()
+            sprite_image_full_path = os.path.join("resources", f"{sprite_filename}.{self.sprite_extension}")
+            surface = scale(load(sprite_image_full_path), (self.width, self.height))
+            surface.set_colorkey(self.color_key)
+            self.image[sprite_filename] = surface
+        self.rect = self.image[self.sprite_filenames[0]].get_rect()
         self.rect.topleft = topleft_x, topleft_y
 
+
     def draw(self, surface: Surface) -> None:
-        surface.blit(self.image, (self.rect.x, self.rect.y))
+        if self.current_image is None:
+            surface.blit(self.image, (self.rect.x, self.rect.y))
+        else:
+            surface.blit(self.image[self.current_image], (self.rect.x, self.rect.y))
 
     def is_collided_with(self, another_object: "GameObject") -> bool:
         return self.rect.colliderect(another_object.rect)
--- main.py
+++ main.py
@@ -7,17 +7,21 @@ from game_object import GameObject
 from text import Text
 
 class Player(GameObject):
-    sprite_filename = ["PacMan", "PacMan_R1", "PacMan_R2", "PacMan_L1", "PacMan_L2", "Pacman_U1", "PacMan_U2", "PacMan_D1", "PacMan_D2"]
+    sprite_filenames = ["PacMan_R1", "PacMan_R2", "PacMan_R3", "PacMan_R4", "PacMan_L1", "PacMan_L2", "PacMan_L3",\
+                        "PacMan_L4", "Pacman_U1", "PacMan_U2", "Pacman_U3", "PacMan_U4","PacMan_D1", "PacMan_D2"\
+                        "PacMan_D3", "PacMan_D4"]
     width = 20
     height = 20
+    current_image = "PacMan_R1"
 
 
 class Wall(GameObject):
-    sprite_filename = "wall"
-
+    sprite_filenames = ["wall"]
+    current_image = "wall"
 
 class Chest(GameObject):
-    sprite_filename = "chest"
+    sprite_filenames = ["wall"]
+    current_image = "wall"
 
 
 def calculate_walls_coordinates(screen_width, screen_height, wall_block_width, wall_block_height):
@@ -52,9 +56,9 @@ def compose_context(screen):
 def draw_whole_screen(screen, context):
     screen.fill("purple")
     context["player"].draw(screen)
-    context["walls"].draw(screen)
-    context["chest"].draw(screen)
-    Text(str(context["score"]), (10, 10)).draw(screen)
+    #context["walls"].draw(screen)
+    #context["chest"].draw(screen)
+    #Text(str(context["score"]), (10, 10)).draw(screen)
 
 
 def main():
@@ -79,12 +83,16 @@ def main():
         old_player_topleft = context["player"].rect.topleft
         if keys[pygame.K_w]:
             context["player"].rect = context["player"].rect.move(0, -1 * player_speed)
+            context["player"].current_image = get_next_img_packman("up")
         if keys[pygame.K_s]:
             context["player"].rect = context["player"].rect.move(0, player_speed)
+            context["player"].current_image = get_next_img_packman("down")
         if keys[pygame.K_a]:
             context["player"].rect = context["player"].rect.move(-1 * player_speed, 0)
+            context["player"].current_image = get_next_img_packman("left")
         if keys[pygame.K_d]:
             context["player"].rect = context["player"].rect.move(player_speed, 0)
+            context["player"].current_image = get_next_img_packman("right")
 
         if spritecollide(context["player"], context["walls"], dokill=False):
             context["player"].rect.topleft = old_player_topleft

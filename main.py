96ed492b0f4e84e464c0691f084dd8b9b3ec7ebb
Denis Kondrashov <dkond@mail.ru>
2023-11-26 22:39:59 +0300

    wall temp

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from text import Text
 
 class Player(GameObject):
     sprite_filenames = ["PacMan_R1", "PacMan_R2", "PacMan_R3", "PacMan_R4", "PacMan_L1", "PacMan_L2", "PacMan_L3",\
-                        "PacMan_L4", "Pacman_U1", "PacMan_U2", "Pacman_U3", "PacMan_U4","PacMan_D1", "PacMan_D2"\
+                        "PacMan_L4", "PacMan_U1", "PacMan_U2", "PacMan_U3", "PacMan_U4","PacMan_D1", "PacMan_D2",\
                         "PacMan_D3", "PacMan_D4"]
     width = 20
     height = 20
@@ -23,6 +23,16 @@ class Chest(GameObject):
     sprite_filenames = ["wall"]
     current_image = "wall"
 
+def get_next_img_packman(current_image: str, direction: str) -> str:
+    old_direct = current_image[7:]
+    if old_direct[0:1] == direction[0:1]:
+        step = int(old_direct[1:]) + 1
+        if step > 4:
+            step = 1
+        current_image = current_image[0:8] + str(step)
+    else:
+        current_image = current_image[0:7] + str(direction[0:1]) + "1"
+    return current_image
 
 def calculate_walls_coordinates(screen_width, screen_height, wall_block_width, wall_block_height):
     horizontal_wall_blocks_amount = screen_width // wall_block_width
@@ -66,7 +76,7 @@ def main():
     screen = pygame.display.set_mode((640, 480))
     clock = pygame.time.Clock()
     running = True
-    player_speed = 1
+    player_speed = 5
 
     context = compose_context(screen)
 
@@ -83,16 +93,16 @@ def main():
         old_player_topleft = context["player"].rect.topleft
         if keys[pygame.K_w]:
             context["player"].rect = context["player"].rect.move(0, -1 * player_speed)
-            context["player"].current_image = get_next_img_packman("up")
+            context["player"].current_image = get_next_img_packman(context["player"].current_image,"Up")
         if keys[pygame.K_s]:
             context["player"].rect = context["player"].rect.move(0, player_speed)
-            context["player"].current_image = get_next_img_packman("down")
+            context["player"].current_image = get_next_img_packman(context["player"].current_image, "Down")
         if keys[pygame.K_a]:
             context["player"].rect = context["player"].rect.move(-1 * player_speed, 0)
-            context["player"].current_image = get_next_img_packman("left")
+            context["player"].current_image = get_next_img_packman(context["player"].current_image, "Left")
         if keys[pygame.K_d]:
             context["player"].rect = context["player"].rect.move(player_speed, 0)
-            context["player"].current_image = get_next_img_packman("right")
+            context["player"].current_image = get_next_img_packman(context["player"].current_image, "Right")
 
         if spritecollide(context["player"], context["walls"], dokill=False):
             context["player"].rect.topleft = old_player_topleft
@@ -104,7 +114,7 @@ def main():
                 random.randint(Wall.height, screen.get_height() - Wall.height * 2),
             )
 
-        clock.tick(120) / 1000
+        clock.tick(18) / 1000
 
     pygame.quit()
 

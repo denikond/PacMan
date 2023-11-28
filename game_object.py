ac5718389a0de3ced8f5021a7d8858aa81d31396
fox <fox@kond.me>
2023-11-28 22:18:39 +0300

    animated encapsulated fro gameobject

--- game_object.py
+++ game_object.py
@@ -24,23 +24,16 @@ class GameObject(Sprite):
 
     def draw(self, surface: Surface) -> None:
         surface.blit(self.image, (self.rect.x, self.rect.y))
-        print(surface)
-
 
     def is_collided_with(self, another_object: "GameObject") -> bool:
         return self.rect.colliderect(another_object.rect)
 
-class AnimatedGameObject(Sprite):
+
+class AnimatedGameObject(GameObject):
     sprite_filenames: list[str] | None = None
-    sprite_extension: str = "png"
-    width: int = 40
-    height: int = 40
-    color_key: tuple[int, int, int] = (245, 245, 245)
-    current_image: str | None = None
 
     def __init__(self, topleft_x: int, topleft_y: int):
-        super().__init__()
-        print(self.sprite_filenames)
+        super(Sprite).__init__()
         self.image = {}
         for sprite_filename in self.sprite_filenames:
             sprite_image_full_path = os.path.join("resources", f"{sprite_filename}.{self.sprite_extension}")
@@ -50,12 +43,8 @@ class AnimatedGameObject(Sprite):
         self.rect = self.image[self.sprite_filenames[0]].get_rect()
         self.rect.topleft = topleft_x, topleft_y
 
-
     def draw(self, surface: Surface) -> None:
-        if self.current_image is None:
-            surface.blit(self.image, (self.rect.x, self.rect.y))
-        else:
-            surface.blit(self.image[self.current_image], (self.rect.x, self.rect.y))
+        surface.blit(self.image[self.current_image], (self.rect.x, self.rect.y))
 
     def is_collided_with(self, another_object: "GameObject") -> bool:
         return self.rect.colliderect(another_object.rect)
